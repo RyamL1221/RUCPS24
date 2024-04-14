dcd2db63a87488e51c897f89e61084aa3e1a85b4
RyamL1221 <ryaml1221@gmail.com>
2024-04-14 11:44:44 -0400

    first commit

---
+++ sorttwonumbers.java
@@ -0,0 +1,73 @@
+import java.io.*;
+import java.util.*;
+ 
+ 
+public class sorttwonumbers{
+   public static void main(String[] args) {
+        MyScanner sc = new MyScanner();
+        out = new PrintWriter(new BufferedOutputStream(System.out));
+        
+        // Start writing your solution here. -------------------------------------
+    
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        
+        if(a > b) {
+            out.println(b + " " + a);
+        } else {
+            out.println(a + " " + b);
+        }
+        // Stop writing your solution here. -------------------------------------
+        out.close();
+   }
+
+     
+
+   //-----------PrintWriter for faster output---------------------------------
+   public static PrintWriter out;
+      
+   //-----------MyScanner class for faster input----------
+   public static class MyScanner {
+      BufferedReader br;
+      StringTokenizer st;
+ 
+      public MyScanner() {
+         br = new BufferedReader(new InputStreamReader(System.in));
+      }
+ 
+      String next() {
+          while (st == null || !st.hasMoreElements()) {
+              try {
+                  st = new StringTokenizer(br.readLine());
+              } catch (IOException e) {
+                  e.printStackTrace();
+              }
+          }
+          return st.nextToken();
+      }
+ 
+      int nextInt() {
+          return Integer.parseInt(next());
+      }
+ 
+      long nextLong() {
+          return Long.parseLong(next());
+      }
+ 
+      double nextDouble() {
+          return Double.parseDouble(next());
+      }
+ 
+      String nextLine(){
+          String str = "";
+	  try {
+	     str = br.readLine();
+	  } catch (IOException e) {
+	     e.printStackTrace();
+	  }
+	  return str;
+      }
+
+   }
+   //--------------------------------------------------------
+}
